1b94453b3c7109760477996c0226e6c26087b583
jaideepghosh <er.jaideepghosh@gmail.com>
2026-09-21 12:02:39 +0530

    feat: add UsersController and corresponding unit tests

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
+import { UsersController } from './users/users.controller.js';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, UsersController],
   providers: [AppService],
 })
 export class AppModule {}
