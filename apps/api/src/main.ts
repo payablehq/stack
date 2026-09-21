86286e3013eecd36bf4e596849205cfca94dd3d5
jaideepghosh <er.jaideepghosh@gmail.com>
2026-09-21 12:01:53 +0530

    feat: enhance API documentation setup with Swagger integration

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module.js';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder()
+    .setTitle('My API')
+    .setDescription('My Monorepo API documentation')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(process.env.PORT ?? 3001);
 }
-await bootstrap();
+
+bootstrap();
