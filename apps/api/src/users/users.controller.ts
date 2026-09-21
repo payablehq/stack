d1ec31a5f699ba90ef2f8426cf3cfbfd75a9cf0f
jaideepghosh <er.jaideepghosh@gmail.com>
2026-09-21 12:02:44 +0530

    feat: implement findAll method in UsersController to retrieve all users

--- apps/api/src/users/users.controller.ts
+++ apps/api/src/users/users.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('users')
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get()
+  @ApiOperation({
+    summary: 'Get all users',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Users successfully retrieved.',
+  })
+  findAll() {
+    return [];
+  }
+}
