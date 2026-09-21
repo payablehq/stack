832f15b60e048c7db9dc42f5130b630d51d34813
jaideepghosh <er.jaideepghosh@gmail.com>
2026-09-21 12:23:42 +0530

    feat: enable CORS for localhost in main.ts

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -15,6 +15,10 @@ async function bootstrap() {
     }),
   );
 
+  app.enableCors({
+    origin: 'http://localhost:3000',
+  });
+
   const config = new DocumentBuilder()
     .setTitle('My API')
     .setDescription('My Monorepo API documentation')
